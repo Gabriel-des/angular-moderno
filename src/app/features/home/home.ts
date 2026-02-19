8b17376fac4ee2b5a04de2457c7289ea5d5d62a1
Gabriel Santos <gabriel.santos@multipedidos.com.br>
2026-02-18 23:49:31 -0300

    - Creates transactiopm interface - Creates render to a transaction - Render a lista of transacations

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,18 +1,21 @@
 import { Component, signal } from '@angular/core';
 import { Balance } from "./components/balance/balance";
+import { TransactionItem } from './components/transaction-item/transaction-item';
+import { Transaction } from '../../shared/transaction/interfaces/transaction';
+import { TransactionType } from '../../shared/transaction/enums/transaction-type';
 
 @Component({
   selector: 'app-home',
-  imports: [Balance],
+  imports: [Balance, TransactionItem],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home {
 
 
-  transactions = signal<{ value: number, type: 'income' | 'outcome' }[]>([
-    { value: 100, type: 'income' },
-    { value: 100, type: 'outcome' },
+  transactions = signal<Transaction[]>([
+    { title: 'Salário', value: 100, type: TransactionType.INCOME },
+    { title: 'Aluguel', value: 100, type: TransactionType.OUTCOME },
   ])
 
 }
