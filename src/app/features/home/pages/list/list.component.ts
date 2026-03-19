fbaa8ffa52c1eedb1eea6206b38b9d52dfa7d524
Gabriel Santos <gabriel.santos@multipedidos.com.br>
2026-03-19 18:11:35 -0300

    refactor: move component home code into features/home/pages/list

---
+++ src/app/features/home/pages/list/list.component.ts
@@ -0,0 +1,71 @@
+import { Component, inject, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink, Router } from '@angular/router';
+import { ConfirmationDialogService } from '../../../../shared/dialog/confirmation/services/confirmation-dialog.service';
+import { FeedbackService } from '../../../../shared/feedback/services/feedback.service';
+import { Transaction } from '../../../../shared/transaction/interfaces/transaction';
+import { TransactionsService } from '../../../../shared/transaction/services/transactions.service';
+import { Balance } from './components/balance/balance';
+import { NoTransactions } from './components/no-transactions/no-transactions';
+import { TransactionItem } from './components/transaction-item/transaction-item';
+import { TransactionsContainerComponent } from './components/transactions-container/transactions-container.component';
+
+@Component({
+  selector: 'app-list',
+  imports: [
+    Balance,
+    TransactionItem,
+    NoTransactions,
+    MatButtonModule,
+    RouterLink,
+    TransactionsContainerComponent,
+  ],
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.scss',
+})
+export class ListComponent {
+  private router = inject(Router);
+  private transactionsService = inject(TransactionsService);
+  private feedbackService = inject(FeedbackService);
+  private confirmationDialogService = inject(ConfirmationDialogService);
+
+  transactions = signal<Transaction[]>([]);
+
+  ngOnInit(): void {
+    this.getTransactions();
+  }
+
+  edit(transaction: Transaction) {
+    this.router.navigate(['/edit', transaction.id]);
+  }
+
+  remove(transaction: Transaction) {
+    this.confirmationDialogService
+      .open({
+        title: 'Deletar transação',
+        message: 'Você realmente quer deletar a transação?',
+      })
+      .subscribe({
+        next: () => {
+          this.transactionsService.delete(transaction.id).subscribe({
+            next: () => {
+              this.removeTransactionFromArray(transaction);
+              this.feedbackService.success('Transação removida com sucesso!');
+            },
+          });
+        },
+      });
+  }
+
+  private removeTransactionFromArray(transaction: Transaction) {
+    this.transactions.update((transactions) =>
+      transactions.filter((item) => item.id !== transaction.id),
+    );
+  }
+
+  private getTransactions() {
+    this.transactionsService.getAll().subscribe({
+      next: (transactions) => this.transactions.set(transactions),
+    });
+  }
+}
